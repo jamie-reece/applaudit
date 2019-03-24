0a30929a1e0581ee70ad423a4308b24ea4697fe4
Jamie Bowman <jamiereecebowman@gmail.com>
2019-03-24 13:30:33 +0000

    update to-do list

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,16 @@
+---
+---
+
 // add active class dynamically to navlinks based on current url
 
-$(function(){
-  var current = location.pathname;
-  $('#nav-item a').each(function(){
-      var $this = $(this);
-      // if the current path is like this link, make it active
-      if($this.attr('href').indexOf(current) !== -1){
-          $this.addClass('active');
-      }
-  })
-})
+// $(function(){
+//   var current = location.pathname;
+//   var permalink = "{{ page.permalink }}"
+//   $('#nav-item a').each(function(){
+//       var $this = $(this);
+//       // if the current path is like this link, make it active
+//       if($this.attr('href').indexOf(current) !== -1){
+//           $this.addClass('active');
+//       }
+//   })
+// })
