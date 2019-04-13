916662d1f786a1c652166612621e0821bc9e1298
Jamie Bowman <jamiereecebowman@gmail.com>
2019-04-13 23:04:40 +0100

    make sidenav responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
-// add active class dynamically to navlinks based on current url
+// add active class dynamically to nav-items based on current url
 
-$(function(){
+$(function addActiveCLass() {
   // returns basePath ignoring extra links i.e. /reviews/~~new-page~~
   var basePath = '/' + window.location.pathname.split('/', 3).filter(Boolean).join('/') + '/';
   // adds active class to items w/ corresponding url
-  $('li#nav-item a[href="' + basePath + '"]').addClass('active');
-})
+  $('li#nav-item a[href="' + basePath + '"]').parent('li').addClass('active');
+});
+
+//
+
+$(function toggleSidenav() {
+  $('a#menu-trigger').click(function() {
+    $('div#side-nav-container').toggleClass('is-expanded');
+    $('div#page-content-container').toggleClass('has-overlay');
+  });
+});
